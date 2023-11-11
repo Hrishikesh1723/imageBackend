9c12bf59d1040abfdcf9ea95b081dd07e9aece50
Hrishikesh1723 <hrishikesh.r@ahduni.edu.in>
2023-11-11 17:29:56 +0530

    first commit

--- python/color.py
+++ python/color.py
@@ -6,12 +6,7 @@ import base64
 import json
 import sys
 
-def extract_colors_from_image_base64(image_base64, num_colors):
-    # Decode the base64 image data
-    image_data = base64.b64decode(image_base64)
-    image_np = np.frombuffer(image_data, np.uint8)
-    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-    
+def extract_colors_from_image(image, num_colors):
     # Convert the image to HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
@@ -23,24 +18,19 @@ def extract_colors_from_image_base64(image_base64, num_colors):
     kmeans.fit(pixels)
     
     # Calculate the percentage of each color cluster in the image
-    cluster_centers = kmeans.cluster_centers_
-    labels = kmeans.labels_
-    
-    # Count the number of pixels in each cluster
-    counts = np.bincount(labels)
+    counts = np.bincount(kmeans.labels_)
     
     # Get the percentage of each color
     percentages = (counts / counts.sum()) * 100
     
     # Sort the colors by percentage
-    color_data = [(percent, color) for percent, color in zip(percentages, cluster_centers)]
-    color_data.sort(reverse=True)
+    color_data = sorted(zip(percentages, kmeans.cluster_centers_), key=lambda x: x[0], reverse=True)
     
     return color_data
 
-def calculate_color_similarity_from_base64(image_base641, image_base642, num_colors):
-    colors1 = extract_colors_from_image_base64(image_base641, num_colors)
-    colors2 = extract_colors_from_image_base64(image_base642, num_colors)
+def calculate_color_similarity(image1, image2, num_colors):
+    colors1 = extract_colors_from_image(image1, num_colors)
+    colors2 = extract_colors_from_image(image2, num_colors)
     
     # Extract color histograms
     hist1 = [color[0] for color in colors1]
@@ -55,6 +45,7 @@ def calculate_color_similarity_from_base64(image_base641, image_base642, num_col
 def calculate_similarity_percentage(emd_distance, max_distance):
     # Calculate the similarity percentage
     similarity_percentage = 100 * (1 - (emd_distance / max_distance))
+    
     return similarity_percentage
 
 if __name__ == "__main__":
@@ -66,14 +57,16 @@ if __name__ == "__main__":
 
     max_possible_distance = 10.0
 
-    similarity_score = calculate_color_similarity_from_base64(image_base64_1, image_base64_2, num_colors)
+    image1 = cv2.imdecode(np.frombuffer(base64.b64decode(image_base64_1), np.uint8), cv2.IMREAD_COLOR)
+    image2 = cv2.imdecode(np.frombuffer(base64.b64decode(image_base64_2), np.uint8), cv2.IMREAD_COLOR)
+
+    similarity_score = calculate_color_similarity(image1, image2, num_colors)
     similarity_percentage = calculate_similarity_percentage(similarity_score, max_possible_distance)
 
     # Output the results as a JSON object
     result = {
         'similarity_score': similarity_score,
-        'similarity_percentage': similarity_percentage,
-        'dkfk9ff0':500
+        'similarity_percentage': similarity_percentage
     }
     json.dumps(result)
     print(json.dumps(result))
